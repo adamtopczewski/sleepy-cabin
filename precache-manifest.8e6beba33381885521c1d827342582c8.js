9ae7d8d0373bbe77c0ee70e2a2e073c7b936402e
adamtopczewski <gnomowisko@gmail.com>
2020-05-03 13:50:01 +0200

    Updates

---
+++ precache-manifest.8e6beba33381885521c1d827342582c8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0bf6f347513959fbbefc3ed0360a3a",
+    "url": "/sleepy-cabin/index.html"
+  },
+  {
+    "revision": "92ba5fed2e0ecf3e3eaf",
+    "url": "/sleepy-cabin/static/css/main.09a7f908.chunk.css"
+  },
+  {
+    "revision": "6b38f543e75641afcb41",
+    "url": "/sleepy-cabin/static/js/2.885eed7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sleepy-cabin/static/js/2.885eed7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ba5fed2e0ecf3e3eaf",
+    "url": "/sleepy-cabin/static/js/main.8695008b.chunk.js"
+  },
+  {
+    "revision": "96db435359428753e9f6",
+    "url": "/sleepy-cabin/static/js/runtime-main.47fb8632.js"
+  },
+  {
+    "revision": "21af3480ebab299094dc15ee3f7aabdf",
+    "url": "/sleepy-cabin/static/media/Joe Font.21af3480.ttf"
+  },
+  {
+    "revision": "ec1d68d60a8a1b224a16a57e70c37bbb",
+    "url": "/sleepy-cabin/static/media/Old Press.ec1d68d6.ttf"
+  },
+  {
+    "revision": "a9fa4b16c2dd0055ee7f097f4aa9be94",
+    "url": "/sleepy-cabin/static/media/circle.a9fa4b16.png"
+  },
+  {
+    "revision": "0865f672ec562853ec0b65b9b64251ae",
+    "url": "/sleepy-cabin/static/media/gravitation_falls.0865f672.ttf"
+  },
+  {
+    "revision": "c3d7a3b44d5776878bec022d84ea402c",
+    "url": "/sleepy-cabin/static/media/heroBg.c3d7a3b4.jpg"
+  },
+  {
+    "revision": "f6558ded9132841145c758ced96eeeca",
+    "url": "/sleepy-cabin/static/media/logo.f6558ded.png"
+  },
+  {
+    "revision": "621097d3794b063d84005e29c2ef980e",
+    "url": "/sleepy-cabin/static/media/note.621097d3.png"
+  },
+  {
+    "revision": "8c40f96e588361af3c5f5d3f794275f6",
+    "url": "/sleepy-cabin/static/media/polaroid1.8c40f96e.png"
+  },
+  {
+    "revision": "69d82ba2ca6704a4c1722da5a40fce27",
+    "url": "/sleepy-cabin/static/media/polaroid2.69d82ba2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6d40973a21bd8df72d775b00e1bcb36.js"
+  "/sleepy-cabin/precache-manifest.8e6beba33381885521c1d827342582c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sleepy-cabin/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
