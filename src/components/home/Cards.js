2b428b671c87aa4e24bba51b2872c72fc1b8b2f5
adamtopczewski <gnomowisko@gmail.com>
2020-05-07 14:14:17 +0200

    fixing components and styling

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../../assets/polaroid1.png'
 import img2 from '../../assets/polaroid2.png'
+// import titleBg from '../../assets/titleSign.png'
 
 import './Cards.css'
 
@@ -10,7 +11,9 @@ export default function Cards() {
             <article className="welcome-card">                
                 <div className="text-with-photo">
                     <div className="card-text">
-                    <h1 className='title'>Welcome in sleepy cabins</h1>
+                        <div className="title-container">                            
+                            <h1 className='title'>Welcome in sleepy cabins</h1>
+                        </div>                    
                     <p className="description">Set in the middle of a 2,600 nature preserve and surrounded by Appalachian Mountains, rustic and historic Mountain Lake Lodge is the ultimate retreat. 
                     Breathe in crisp, fresh air, dine on gourmet fare and build family memories with fun indoor and outdoor activities.</p>
                     </div>
@@ -21,7 +24,9 @@ export default function Cards() {
 
                 <div className="text-with-photo row-reverse">
                     <div className="card-text">
-                        <h1 className='title'>And enjoy your stay</h1>
+                        <div className="title-container sign-reverse">  
+                            <h1 className='title'>And enjoy your stay</h1>
+                        </div>
                         <p className="description">We offer a variety of accommodations, from rustic cabins and cozy cottages to lodging in our historic Main Stone Lodge. 
                         Expansive meeting and event venues set the stage for romantic weddings and relaxing business events. 
                         Arrive to an unhurried pace and enjoy adventure and comfort in the heart of Virginia's Blue Ridge Mountains.</p>
--- src/components/home/FeaturedRooms.js
+++ src/components/home/FeaturedRooms.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import room1 from '../../assets/Cabins/mock1.png'
+import room2 from '../../assets/Cabins/mock2.png'
+import room3 from '../../assets/Cabins/mock3.png'
 
 import './FeaturedRooms.css'
 
 export default function FeaturedRooms() {
+    
     return (
-        <div>
-            {/* TBD */}
-        </div>
+        <>
+            <header className="featured-header">
+                <h1>Featured Rooms</h1>
+            </header>
+            <div className="container">
+                <section className="featured-rooms">
+                    <a className="room" href="">
+                        <img src={room1} className="room-img"/>
+                        <p className="room-name">Cabin 1</p>
+                    </a>
+                    <a className="room" href="">
+                        <img src={room2} className="room-img"/>
+                        <p className="room-name">Cabin 2</p>
+                    </a>    
+                    <a className="room" href="">
+                        <img src={room3} className="room-img"/>
+                        <p className="room-name">Cabin 3</p>
+                    </a>                       
+                </section>
+            </div>
+        </>
     )
 }
