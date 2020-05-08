872ae46ef460ea38880de2eeed87d54d9f63bce8
adamtopczewski <gnomowisko@gmail.com>
2020-05-08 13:54:39 +0200

    added skeleton for rest of pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,1 +1,14 @@
 /* TBD */
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function About() {
+    return (
+        <>
+            <Navbar />
+
+            <Footer />
+        </>
+    )
+}
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,1 +1,11 @@
 // TBD - page not found 404
+
+export default function Error() {
+    return (
+        <>
+            <Navbar />
+
+            <Footer />
+        </>
+    )
+}
