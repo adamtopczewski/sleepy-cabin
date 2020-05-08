6c30b720e18b6730aa848bd2c9b3b75f2632b64c
adamtopczewski <gnomowisko@gmail.com>
2020-05-08 13:53:36 +0200

    added context and data(tbd)

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export default [
+    {
+      id: "1",
+      name: "double cabin",
+      slug: "double-cabin",
+      type: "double",
+      price: 350,
+      size: 200,
+      capacity: 2,
+      pets: true,
+      featured: true,
+      description:
+      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+      extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+      ],
+        images: [
+          {
+              url: img1
+          },
+          {
+              url: room2
+          },    
+        ]
+      },
+      {
+          id: "2",
+          name: "family",
+          slug: "family",
+          type: "family",
+          price: 500,
+          size: 200,
+          capacity: 4,
+          pets: true,
+          featured: true,
+          description:
+          "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+          extras: [
+          "Plush pillows and breathable bed linens",
+          "Soft, oversized bath towels",
+          "Full-sized, pH-balanced toiletries",
+          "Complimentary refreshments",
+          "Adequate safety/security",
+          "Internet",
+          "Comfortable beds"
+          ],
+            images: [
+              {
+                  url: img1
+              },
+              {
+                  url: room2
+              },    
+            ]
+          },  
+          {
+              id: "3",
+              name: "party exclusive",
+              slug: "party-exclusive",
+              type: "exclusive",
+              price: 10000,
+              size: 200,
+              capacity: 20,
+              pets: true,
+              featured: true,
+              description:
+              "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
+              extras: [
+              "Plush pillows and breathable bed linens",
+              "Soft, oversized bath towels",
+              "Full-sized, pH-balanced toiletries",
+              "Complimentary refreshments",
+              "Adequate safety/security",
+              "Internet",
+              "Comfortable beds"
+              ],
+                images: [
+                  {
+                      url: img1
+                  },
+                  {
+                      url: room2
+                  },    
+                ]
+              },
+  ];
