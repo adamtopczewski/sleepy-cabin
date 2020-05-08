c177a1c2db26f8a1714dd1338bb8e6f21a70c603
adamtopczewski <gnomowisko@gmail.com>
2020-05-08 15:16:37 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 // only navbar and switch w/ routes
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 // Router setup
-import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import Cabins from './pages/Cabins'
+import About from './pages/About'
+import SingleCabin from './pages/SingleCabin'
+import Error from './pages/Error'
 
 function App() {
   return (
     <>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/cabins/" exact component={Cabins} />
+      <Route path="/about/" exact component={About} />
+      <Route path="/cabins/:slug" exact component={SingleCabin} />
+      <Route component={Error}/>
     </Switch>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
         <header className="header">
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li className="nav-item">Home</li>
-                    <li className="nav-item">Cabins</li>
-                    <li className="nav-item">About</li>                        
+                    <li className="nav-item"><Link to="/">Home</Link></li>
+                    <li className="nav-item"><Link to="/cabins">Cabins</Link></li>
+                    <li className="nav-item"><Link to="/about">About</Link></li>                        
                 </ul>
             </nav>
     </header>
