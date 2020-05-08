286f5d06b71c628b6dcdd7b4d666d4689e2c8721
adamtopczewski <gnomowisko@gmail.com>
2020-05-08 13:55:14 +0200

    Added Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
 import Hero from '../components/home/Hero'
 import Cards from '../components/home/Cards'
 import FeaturedRooms from '../components/home/FeaturedRooms'
 import Amenities from '../components/home/Amenities'
+
 
 export default function Home() {
     return (
@@ -14,6 +16,7 @@ export default function Home() {
             <Cards />
             <FeaturedRooms />
             <Amenities />
+            <Footer />
         </>
     )
 }
