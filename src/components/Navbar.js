4bd305defd8dd901c6f2b6831071ee96305a1082
adamtopczewski <gnomowisko@gmail.com>
2020-07-24 18:30:48 +0200

    Fixed "Link" text-decoration

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,17 @@ export default function Navbar() {
         <header className="header">
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li className="nav-item"><Link to="/">Home</Link></li>
-                    <li className="nav-item"><Link to="/cabins">Cabins</Link></li>
-                    <li className="nav-item"><Link to="/about">About</Link></li>                        
+                    <li className="nav-item">
+                        <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/cabins" style={{ textDecoration: 'none' }}>Cabins</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/about" style={{ textDecoration: 'none' }}>About</Link>
+                    </li>
                 </ul>
             </nav>
-    </header>
+        </header>
     )
 }
