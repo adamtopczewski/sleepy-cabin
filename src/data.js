600aeff812c7a1ef04b7db4d46d70294b909774b
adamtopczewski <gnomowisko@gmail.com>
2020-07-24 18:29:14 +0200

    added rooms up to 6

--- src/data.js
+++ src/data.js
@@ -1,17 +1,20 @@
 export default [
-    {
-      id: "1",
-      name: "double cabin",
-      slug: "double-cabin",
-      type: "double",
-      price: 350,
-      size: 200,
-      capacity: 2,
-      pets: true,
-      featured: true,
-      description:
-      "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-      extras: [
+  {
+    id: "1",
+    name: "double cabin",
+    slug: "double-cabin",
+    type: "double",
+    price: 350,
+    size: 150,
+    capacity: 2,
+    pets: false,
+    featured: true,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+
+      Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+      
+      In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
       "Plush pillows and breathable bed linens",
       "Soft, oversized bath towels",
       "Full-sized, pH-balanced toiletries",
@@ -19,74 +22,179 @@ export default [
       "Adequate safety/security",
       "Internet",
       "Comfortable beds"
-      ],
-        images: [
-          {
-              url: img1
-          },
-          {
-              url: room2
-          },    
-        ]
+    ],
+    images: [
+      {
+        url: img1
       },
       {
-          id: "2",
-          name: "family",
-          slug: "family",
-          type: "family",
-          price: 500,
-          size: 200,
-          capacity: 4,
-          pets: true,
-          featured: true,
-          description:
-          "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-          extras: [
-          "Plush pillows and breathable bed linens",
-          "Soft, oversized bath towels",
-          "Full-sized, pH-balanced toiletries",
-          "Complimentary refreshments",
-          "Adequate safety/security",
-          "Internet",
-          "Comfortable beds"
-          ],
-            images: [
-              {
-                  url: img1
-              },
-              {
-                  url: room2
-              },    
-            ]
-          },  
-          {
-              id: "3",
-              name: "party exclusive",
-              slug: "party-exclusive",
-              type: "exclusive",
-              price: 10000,
-              size: 200,
-              capacity: 20,
-              pets: true,
-              featured: true,
-              description:
-              "Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.",
-              extras: [
-              "Plush pillows and breathable bed linens",
-              "Soft, oversized bath towels",
-              "Full-sized, pH-balanced toiletries",
-              "Complimentary refreshments",
-              "Adequate safety/security",
-              "Internet",
-              "Comfortable beds"
-              ],
-                images: [
-                  {
-                      url: img1
-                  },
-                  {
-                      url: room2
-                  },    
-                ]
-              },
-  ];
+        url: room2
+      },
+    ]
+  },
+  {
+    id: "2",
+    name: "family small",
+    slug: "family-small",
+    type: "family",
+    price: 500,
+    size: 200,
+    capacity: 4,
+    pets: true,
+    featured: true,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+
+          Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+      
+          In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+    ],
+    images: [
+      {
+        url: img1
+      },
+      {
+        url: room2
+      },
+    ]
+  },
+  {
+    id: "3",
+    name: "party exclusive",
+    slug: "party-exclusive",
+    type: "exclusive",
+    price: 10000,
+    size: 600,
+    capacity: 20,
+    pets: false,
+    featured: true,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+
+              Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+      
+              In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+    ],
+    images: [
+      {
+        url: img1
+      },
+      {
+        url: room2
+      },
+    ]
+  },
+  {
+    id: "4",
+    name: "family big",
+    slug: "family-big",
+    type: "family",
+    price: 500,
+    size: 300,
+    capacity: 6,
+    pets: false,
+    featured: false,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+  
+                Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+        
+                In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+    ],
+    images: [
+      {
+        url: img1
+      },
+      {
+        url: room2
+      },
+    ]
+  },
+  {
+    id: "5",
+    name: "couple",
+    slug: "couple",
+    type: "double",
+    price: 450,
+    size: 200,
+    capacity: 2,
+    pets: true,
+    featured: false,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+    
+                  Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+          
+                  In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+    ],
+    images: [
+      {
+        url: img1
+      },
+      {
+        url: room2
+      },
+    ]
+  },
+  {
+    id: "6",
+    name: "party exclusive",
+    slug: "party-exclusive",
+    type: "exclusive",
+    price: 7000,
+    size: 500,
+    capacity: 10,
+    pets: true,
+    featured: false,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit, enim quis rutrum auctor, ligula risus malesuada est, id molestie nisi felis eu lacus. Maecenas sit amet risus sed arcu ullamcorper lacinia sed a lacus. Aliquam quis mauris accumsan, dignissim ex vitae, iaculis diam. Vivamus et purus odio. Aliquam ante leo, congue viverra bibendum at, imperdiet quis nibh. Integer at eros vitae quam interdum pretium. Ut ornare libero in urna ornare, non gravida nibh hendrerit. Vivamus interdum nisi nisl, sit amet mattis risus accumsan sit amet. Morbi vitae pellentesque felis, dictum gravida tortor. Praesent ut nisl dolor. Nullam nisi urna, pulvinar semper efficitur sit amet, pellentesque a odio. Fusce nisi lectus, pulvinar eget velit congue, volutpat sollicitudin lectus. Donec commodo volutpat velit dictum rhoncus.
+      
+                    Nam blandit tellus ut velit scelerisque porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam faucibus turpis in purus euismod lacinia. Integer et commodo lacus. Nulla tincidunt id tellus a commodo. Duis posuere ultricies ante a pellentesque. In vehicula interdum elementum.
+            
+                    In consectetur vulputate ullamcorper. Sed quis erat rhoncus, condimentum orci in, pellentesque est. Curabitur pretium dapibus velit, nec bibendum purus imperdiet vitae. Maecenas sit amet lacus arcu. Morbi placerat ante convallis justo semper, non malesuada odio ornare. Fusce ornare scelerisque commodo. Nulla accumsan pharetra risus, et sagittis leo tristique id. Morbi faucibus in enim ac euismod.`,
+    extras: [
+      "Plush pillows and breathable bed linens",
+      "Soft, oversized bath towels",
+      "Full-sized, pH-balanced toiletries",
+      "Complimentary refreshments",
+      "Adequate safety/security",
+      "Internet",
+      "Comfortable beds"
+    ],
+    images: [
+      {
+        url: img1
+      },
+      {
+        url: room2
+      },
+    ]
+  }
+];
