9398c3756a26f619af31737260f9da2039a65159
adamtopczewski <gnomowisko@gmail.com>
2020-08-03 14:27:30 +0200

    added content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,18 @@
 /* TBD */
 import React from 'react'
+import Hero from '../components/Hero'
 
 export default function About() {
     return (
         <>
-            <h1>About</h1>
+            <Hero renderClass="about" title="about" />
+            <section className="about-text">
+                <h1>
+                    Simple reservation website made in react using create-react-app. This project is a practice in design, animations, routing and working with mock data. <br /><br />
+
+                    Since this is a fictional business all the social links, addresses, and contact information are invalid. Backend is mocked using static data.
+                </h1>
+            </section>
         </>
     )
 }
