0b443aeb4b09262868fc1762b663dcd6c3df5c2d
adamtopczewski <gnomowisko@gmail.com>
2020-08-03 14:28:35 +0200

    added conditional classes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import logo from '../assets/logo.png'
 import './Hero.css'
+import classNames from 'classnames'
 
-export default function Hero() {
-    return (
-        <div className="hero">
+export default function Hero(props) {
+        const conditionalClass = classNames('hero', {
+            'home': props.renderClass === 'home',
+            'cabins': props.renderClass === 'cabins',
+            'singleCabin': props.renderClass === 'singleCabin',
+            'about': props.renderClass === 'about'
+        });
+
+    return (        
+        <div className={conditionalClass}>
             <img src={logo} className="hero-logo"/>
+            <h6 className="hero-title">{props.title}</h6>
             <p className="hero-text">Get your keys now</p>
         </div>
     )
